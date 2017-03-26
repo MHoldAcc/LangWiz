e66798c86b602c3f03731321f279827f9f2f45ed
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-03-26 19:47:09 +0200

    Created login Page (No functionality)

---
+++ Web/dashboard.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michael
+ * Date: 26.03.2017
+ * Time: 17:53
+ */
--- Web/index.php
+++ Web/index.php
@@ -1,1 +1,20 @@
-Hello World
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LangWiz Login</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="/assets/css/default.css">
+</head>
+
+<body class="loginPage">
+    <div class="centeredForm">
+        <img src="/assets/img/placeholder.png">
+        <form method="post" action="/dashboard.php" accept-charset="utf-8">
+            <input type="email" name="mail" placeholder="E-Mail"/>
+            <input type="password" name="password" placeholder="Password"/>
+            <input type="submit" value="Login" name="login" class="login"/>
+        </form>
+        <a href="">Register</a>
+    </div>
+</body>
+</html>
