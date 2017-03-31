6ca162c8ef4bd3642b047dca868ad89231b37e69
LYNDWYN\Laura <laura.steiner@nyp.ch>
2017-03-31 09:29:07 +0200

    pages site and path

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
     <title>LangWiz Login</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="/assets/css/default.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/default.css">
 </head>
 
 <body class="loginPage">
     <div class="centeredForm">
-        <img src="/assets/img/placeholder.png">
+        <img src="assets/img/placeholder.png">
         <form method="post" action="/dashboard.php" accept-charset="utf-8">
             <input type="email" name="mail" placeholder="E-Mail"/>
             <input type="password" name="password" placeholder="Password"/>
