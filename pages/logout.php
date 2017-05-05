5f91c4485dcef5df3379ea7970d42b90bf10b2f8
DESKTOP-I2OVG7C\Drake <kaiden3991@gmail.com>
2017-05-05 11:33:49 +0200

    edited logout

--- pages/logout.php
+++ pages/logout.php
@@ -2,5 +2,5 @@
 	session_start();
 	session_destroy(); // Destroying All Sessions
     echo "You are being loged out.";
-    header("Refresh: 3; Location: ../index.php"); // Redirecting To Home Page
+    header("Refresh: 3; URL=http://localhost/LangWizz/"); // Redirecting To Home Page
 ?>
