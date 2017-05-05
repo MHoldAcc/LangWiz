ea3f5e676f2ce568e813f26df3a2b15e093ea168
DESKTOP-I2OVG7C\Drake <kaiden3991@gmail.com>
2017-05-05 11:28:24 +0200

    edited logout

--- pages/logout.php
+++ pages/logout.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
 	session_destroy(); // Destroying All Sessions
+    echo "You are being loged out.";
     header("Refresh: 3; Location: ../index.php"); // Redirecting To Home Page
 ?>
