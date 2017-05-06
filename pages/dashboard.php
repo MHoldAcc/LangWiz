ec3de0552be114c05db9ad7dc834e89d7419c8eb
ConsoleArgonauts Home PC <vanderheide.michael@gmail.com>
2017-05-06 15:20:49 +0200

    Added normalize.css and startet with dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -10,16 +10,39 @@
  <!DOCTYPE html>
 <html>
 <head>
-    <title>LangWiz Login</title>
+    <title>Dashboard</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="../assets/css/default.css">
 </head>
 
-<body class="loginPage">
-    <div class="centeredForm">
-        <img src="../assets/img/placeholder.png">
-			<p>Welcome</p></br> 
-        <a href="logout.php">Logout</a>
+<body class="page dashboard">
+    <div class="menu">
+        <div class="buttons">
+            <a href="dashboard.php" class="dashboard" title="Dashboard">
+                <img src="../assets/img/placeholder.png"/>
+            </a>
+            <a href="settings.php" class="settings" title="Settings">
+                <img src="../assets/img/placeholder.png"/>
+            </a>
+            <a href="logout.php" class="logout" title="Logout">
+                <img src="../assets/img/placeholder.png"/>
+            </a>
+        </div>
+    </div>
+    <div class="content">
+        <div class="container">
+            <div class="vocab">
+                <h2>Vocabulary Set 1</h2>
+                <div class="symbols">
+                    <a class="edit" title="Edit">
+                        <img src="../assets/img/placeholder.png">
+                    </a>
+                    <a class="learn" title="Learn">
+                        <img src="../assets/img/placeholder.png">
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
