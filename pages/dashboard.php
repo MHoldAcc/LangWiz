7434f6afd61d407654ee47c7a43e2920656d6719
ConsoleArgonauts Home PC <vanderheide.michael@gmail.com>
2017-05-07 17:20:52 +0200

    Fixed auto creation of vocabulary sets

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -35,7 +35,6 @@
 <div class="content col-sm-11">
         <div class="container">
             <div class="placeholder"></div>
-            <div class="vocab">
                 <?php
                 $connection = mysqli_connect("localhost", "root", "", "langwizz"); // Establishing connection with server..
                 $query1 = mysqli_query($connection, "SELECT setID FROM sets WHERE userFK like (select userID from `user` where mail like '".$_COOKIE['mail']."' )");
@@ -44,28 +43,16 @@
                     $query2 = mysqli_query($connection, "SELECT setName FROM sets WHERE userFK like (select userID from `user` where mail like '".$_COOKIE['mail']."' )");
                     $result2 = mysqli_fetch_row($query2);
                     foreach ($result2 as $result) {
+                        echo '<div class="vocab">';
                         echo '<p style="float:left;">' . $i . '.    ' . $result . '</p>';
+                        echo '<div class="symbols" style="float:right;">';
+                        echo '<a class="edit" title="Edit" href="edit?set=' . $result . '">';
+                        echo '<img src="../assets/img/Edit.png"></a>';
+                        echo '<a class="learn" title="Learn" href="learn.php?set=' . $result . '">';
+                        echo '<img src="../assets/img/Play.png"></a></div></div>';
                     }
                 }
                 ?>
-                <div class="symbols" style="float:right;">
-                    <a class="edit" title="Edit" href="edit?set=41">
-                        <img src="../assets/img/Edit.png"></a>
-                    <a class="learn" title="Learn" href="learn.php?set=41">
-                        <img src="../assets/img/Play.png">
-                    </a>
-                </div>
-            </div>
-            <div class="vocab">
-                <p style="float:left;">Vocabulary Set 2</p>
-                <div class="symbols" style="float:right;">
-                    <a class="edit" title="Edit" href="edit?set=42">
-                        <img src="../assets/img/Edit.png"></a>
-                    <a class="learn" title="Learn" href="learn.php?set=42">
-                        <img src="../assets/img/Play.png">
-                    </a>
-                </div>
-            </div>
         </div>
     </div>
 </body>
