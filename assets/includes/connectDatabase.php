585fab153c6e5a7e58ab034ae2cf64a157db9683
VMPAF\Enrico <kaiden3991@gmail.com>
2017-05-08 12:45:58 +0200

    edited editvocab... and connectDB files moved includes to assets

---
+++ assets/includes/connectDatabase.php
@@ -0,0 +1,69 @@
+<?php
+/**
+* Created by PhpStorm.
+* User: Laura
+* Date: 07.05.2017
+* Time: 15:03
+*/
+
+//Code zum ein set anlege
+
+
+/*Führt Code aus nachdem der Erfassen-Button betätigt wurde.*/
+if(!empty($_POST["newWords"])){
+  if($_POST['wordOne'] != "" and $_POST['wordTwo'] != "" ){
+    insertIntoDB($_POST['wordOne'], $_POST['wordTwo'] );
+  }
+}
+
+/*Führt Code aus nachdem der Delete-Button betätigt wurde.*/
+if(!empty($_POST['delete'])) {
+  $connection = mysqli_connect("localhost", "root", "", "langwizz"); // Establishing connection with server..
+  $delete = "delete from words where wordID = ?";
+  deleteFromDB($connection, $delete);
+  $sql = "select * from words";
+  //$sql = "select * from words ";
+  createDropdown($connection, $sql);
+}
+
+
+function insertIntoDB($wordOne, $wordTwo) {
+  $connection = mysqli_connect("localhost", "root", "", "langwizz"); // Establishing connection with server..
+  $query = mysqli_query($connection, "insert into words( word1, word2) values ('".$wordOne."', '".$wordTwo."')");
+  if ($query) {
+    echo "Words Successfully added.....";
+  } else {
+    echo "Error....!!";
+  }
+  mysqli_close($connection);
+}
+
+
+/*
+* Erstellt ein Dropdown indem es den Inhalt der Datenbank ausliest.
+* Das Dropdown DeleteZS wird erstellt und erhÃ¤lt so viele Optionen wie es DatensÃ¤tze in der DB hat.
+* Dieser Funktion muss man keine Parameterwerte mitgeben.
+*/
+function createDropdown($connection, $sql)
+{
+  $result = mysqli_query($connection, $sql);
+  echo '<select  class="form-control"  name= "deleteFromDB">';
+
+  while ($row = $result->fetch_assoc()) {
+    echo '<option value= "' . $row['wordID'] . '">' . "Word one: " . $row['word1'] . " -- Word two: " . $row['word2'] . '</option>';
+  }
+  echo '</select>';
+}
+
+/*
+* LÃ¶scht den Eintrag in der Datenbank
+* Parameterwerte sind die $conn Variable und die Value der delete-Tabelle.
+* Diese Funktion hat keinen Returnwert.
+*/
+function deleteFromDB($connection, $delete)
+{
+  $sql = $connection->prepare($delete);
+  $sql->bind_param("s", $_POST['deleteFromDB']);
+  $sql->execute();
+  $sql->close();
+}
--- pages/editvocabulary.php
+++ pages/editvocabulary.php
@@ -1,93 +1,86 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Michael
- * Date: 26.03.2017
- * Time: 17:53
- */
- ?>
- 
- <!DOCTYPE html>
+* Created by PhpStorm.
+* User: Michael
+* Date: 26.03.2017
+* Time: 17:53
+*/
+?>
+
+<!DOCTYPE html>
 <html>
-<head>
+  <head>
     <title>Edit Voci</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="../assets/css/default.css">
-</head>
+  </head>
 
-<body class="page dashboard">
+  <body class="page dashboard">
+    <?php
+    //include '../includes/connectDatabase.php';
 
-                <?php
-                    include '../includes/connectDatabase.php';
+    //Definition der Variable $conn
+    //$conn = mysqli_connect("localhost", "nypadmin", "!30nyp48", "langwizz");
+    //mysqli_query($conn, "SET NAMES 'utf8'");
 
-                    //Definition der Variable $conn
-                    $conn = mysqli_connect("localhost", "nypadmin", "!30nyp48", "langwizz");
-                    mysqli_query($conn, "SET NAMES 'utf8'");
+    //if (!$conn) {
+    //    die("FEHLER, keine Verbindung zur Datenbank m&ouml;glich: " . mysqli_connect_error());
+    //}
+    ?>
+    <div class="editVocabulary">
+      <br><br>
+      <h3>Add a new Set</h3>
+      <!--<form method="post" action="editvocabulary.php" accept-charset="utf-8">-->
+      <form method="post" action="../assets/includes/connectDatabase.php" accept-charset="utf-8">
+        Set name:
+        <input type="text" name="setName" placeholder="Set 1"/><br>
+        Language One:
+        <input type="text" name="languageOne" placeholder="Example: Deutsch"/><br>
+        Language Two:
+        <input type="text" name="languageTwo" placeholder="Example: Englisch"/><br>
+        <input type="submit" value=" Ok " name="newSet" class="newLanguages"/>
+      </form>
+      <br><br>
+      <h3>Add new Words</h3>
+      <!--<form method="post" action="editvocabulary.php" accept-charset="utf-8">-->
+      <form method="post" action="../assets/includes/connectDatabase.php" accept-charset="utf-8">
+        Word One:
+        <input type="text" class="form-control" name="wordOne" placeholder="Example: das Licht" /><br>
+        Word Two:
+        <input type="text" class="form-control" name="wordTwo" placeholder="Example: light" /><br>
+        <input type="submit" value=" Ok " name="newWords" class="newWords"/>
+      </form>
+      <?php
+      /* $error= "";
+      /*Führt Code aus nachdem der Erfassen-Button betätigt wurde.*
+      if(!empty($_POST["newWords"]))
+      {
+      if($_POST['wordOne'] != "" and $_POST['wordTwo'] != "" )
+      {
+      insertIntoDB($_POST['wordOne'], $_POST['wordTwo'] );
+      }
+      }*/
+      ?>
+      <br><br>
+      <h3>Delete Words</h3>
+      <!--<form method="post" action="editvocabulary.php">-->
+      <form action="../assets/includes/connectDatabase.php" method="post">
 
-                    if (!$conn) {
-                        die("FEHLER, keine Verbindung zur Datenbank m&ouml;glich: " . mysqli_connect_error());
-                    }
-                ?>
+        <?php
+        /*$sql = "select * from words";
+        $result = mysqli_query($conn, $sql);
 
-                <div class="editVocabulary">
-                    <br><br>
-                    <h3>Add a new Set</h3>
-                    <form method="post" action="editvocabulary.php" accept-charset="utf-8">
-                        Set name:
-                        <input type="text" name="setName" placeholder="Set 1"/><br>
-                        Language One:
-                        <input type="text" name="languageOne" placeholder="Example: Deutsch"/><br>
-                        Language Two:
-                        <input type="text" name="languageTwo" placeholder="Example: Englisch"/><br>
-                        <input type="submit" value=" Ok " name="newSet" class="newLanguages"/>
-                    </form>
-
-                    <br><br>
-                    <h3>Add new Words</h3>
-                    <form method="post" action="editvocabulary.php" accept-charset="utf-8">
-                        Word One:
-                        <input type="text" class="form-control" name="wordOne" placeholder="Example: das Licht" /><br>
-                        Word Two:
-                        <input type="text" class="form-control" name="wordTwo" placeholder="Example: light" /><br>
-                        <input type="submit" value=" Ok " name="newWords" class="newWords"/>
-                    </form>
-
-                    <?php
-                        $error= "";
-                        /*Führt Code aus nachdem der Erfassen-Button betätigt wurde.*/
-                        if(isset($_POST["newWords"]))
-                        {
-                            if($_POST['wordOne'] != "" and $_POST['wordTwo'] != "" )
-                            {
-                                insertIntoDB($_POST['wordOne'], $_POST['wordTwo'] );
-                            }
-                        }
-                    ?>
-
-
-                    <br><br>
-                    <h3>Delete Words</h3>
-
-                    <form action="editvocabulary.php" method="post">
-
-                        <?php
-                                    $sql = "select * from words";
-                                    $result = mysqli_query($conn, $sql);
-
-                                /*Führt Code aus nachdem der Delete-Button betätigt wurde.*/
-                                if(isset($_POST['delete'])) {
-                                        $delete = "delete from words where wordID = ?";
-                                        deleteFromDB($conn, $delete);
-                                    }
-                                $sql = "select * from words ";
-                                createDropdown($conn, $sql);
-                            ?>
-                        <br>
-                        <button type="submit" name="delete">Delete</button>
-                    </form>
-                </div>
-
-        </div>
+        /*Führt Code aus nachdem der Delete-Button betätigt wurde.*
+        if(!empty($_POST['delete'])) {
+          $delete = "delete from words where wordID = ?";
+          deleteFromDB($conn, $delete);
+        }
+        $sql = "select * from words ";
+        createDropdown($conn, $sql);*/
+        ?>
+        <br>
+        <button type="submit" name="delete">Delete</button>
+      </form>
     </div>
-</body>
+  </body>
 </html>
