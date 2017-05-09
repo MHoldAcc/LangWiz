0c9570319ca3b59990205f9b2d05cc18d22145af
ConsoleArgonauts Home PC <vanderheide.michael@gmail.com>
2017-05-09 20:35:58 +0200

    Changed some styling issues

--- assets/includes/defaultfunctions.php
+++ assets/includes/defaultfunctions.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
     //Redirects to index if user is not set
-    //if(empty($_SESSION['login_user'])){header("Refresh: 0; URL=../index.php"); }
+    //if(empty($_SESSION['login_user'])){header("URL=../index.php"); }
 ?>
